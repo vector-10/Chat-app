cfb3e511f76ca7c0e3539f00e047dbd00d955183
Vector-9 <97702819+Vector-9@users.noreply.github.com>
2023-05-29 11:28:47 +0100

    deployment

--- client/vectorware-chat/src/App.js
+++ client/vectorware-chat/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Chat from './components/Chat';
 import Home from './components/Home';
 
-const socket = socketIO.connect('https://chat-app-vector-10.vercel.app/')
+const socket = socketIO.connect('https://chat-server-2ear.onrender.com/')
 
 function App() {
   return (
